ba2ca2daf545d1f2010c9caa8e5787074b1e8946
James Steele <james.steele@solent.ac.uk>
2024-07-10 07:40:24 +0100

    Update from MyJump2 to My Jump Lab

--- R/functions.R
+++ R/functions.R
@@ -270,7 +270,7 @@ make_agree_plot <- function(data, agree_models) {
   }
   
   (plots$jmc + labs(title = "Just Jump System")) +
-    (plots$mj + labs(title = "MyJump App")) +
+    (plots$mj + labs(title = "My Jump Lab")) +
     (plots$opto + labs(title = "Optojump")) +
         plot_annotation(title = "Agreement Between Methods",
                     subtitle = "Mixed Effects Model Mean Bias and 95% Limits of Agreement",
@@ -381,7 +381,7 @@ make_reli_plot <- function(data, reli_models) {
   
   (plots$fdi + labs(title = "Force Decks")) +
     (plots$jmc + labs(title = "Just Jump System")) +
-    (plots$mj + labs(title = "MyJump App")) +
+    (plots$mj + labs(title = "My Jump Lab")) +
     (plots$opto + labs(title = "Optojump")) + 
     plot_annotation(title = "Test-Retest Reliability Across Methods",
                     subtitle = "Mixed Effects Model Mean Bias and 95% Limits of Agreement",
